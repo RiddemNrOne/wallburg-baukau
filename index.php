2ae6035d4d9aecc43442efb361b48801bef514b8
Johannes <JBokelmann@hotmail.de>
2025-02-23 18:37:06 +0100

    Auslagerung der Header & Navbar

--- index.php
+++ index.php
@@ -1,28 +1,7 @@
 <!doctype html>
 <html lang="de">
 <head>
-    <meta charset="utf-8">
-    <title>Wallburg Baukau</title>
-    <meta name="author" content="Johannes Bokelmann">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="ico64.ico" rel="shortcut icon" type="image/x-icon">
-    <link href="wb.png" rel="icon" sizes="482x631" type="image/png">
-    <link href="font-awesome.min.css" rel="stylesheet">
-    <link href="WB.css" rel="stylesheet">
-    <link href="index.css" rel="stylesheet">
-    <script src="jquery-1.12.4.min.js"></script>
-    <script src="jquery-ui.min.js"></script>
-    <script src="wb.panel.min.js"></script>
-    <script src="wwb15.min.js"></script>
-    <script>
-    $(document).ready(function() {
-        // Smooth scrolling to anchors
-        $("a[href*='#PageHeader']").click(function(event) {
-            event.preventDefault();
-            $('html, body').stop().animate({ scrollTop: $('#wb_PageHeader').offset().top - 100 }, 600, 'swing');
-        });
-    });
-    </script>
+    <?php include 'header.php'; ?>
 </head>
 <body>
     <!-- Header -->
@@ -33,18 +12,7 @@
     </div>
 
     <!-- Navigation -->
-    <nav id="wb_PanelMenu2">
-        <ul role="menu">
-            <li role="menuitem"><a href="./index.php" class="active"><i class="fa fa-home fa-fw"></i> Wallburg Baukau</a></li>
-            <li role="menuitem"><a href="./gruppe.php"><i class="fa fa-user fa-fw"></i> Das sind wir</a></li>
-            <li role="menuitem"><a href="./termine.php"><i class="fa fa-calendar-check-o fa-fw"></i> Unsere Termine</a></li>
-            <li role="menuitem"><a href="./ausrüstung.php"><i class="fa fa-cubes fa-fw"></i> Die Ausrüstung</a></li>
-            <li role="menuitem"><a href="./historisches.php"><i class="fa fa-graduation-cap fa-fw"></i> Historisches</a></li>
-            <li role="menuitem"><a href="./social.php"><i class="fa fa-camera fa-fw"></i> Social Media</a></li>
-            <li role="menuitem"><a href="./wif/feuer.php"><i class="fa fa-thermometer-full fa-fw"></i> Wallburg in Flammen</a></li>
-            <li role="menuitem"><a href="./impressum.php"><i class="fa fa-hashtag fa-fw"></i> Impressum</a></li>
-        </ul>
-    </nav>
+    <?php include 'nav.php'; ?>
 
     <!-- Hauptinhalt -->
     <main>
---
+++ nav.php
@@ -0,0 +1,12 @@
+<nav id="wb_PanelMenu2">
+        <ul role="menu">
+            <li role="menuitem"><a href="./index.php" class="active"><i class="fa fa-home fa-fw"></i> Wallburg Baukau</a></li>
+            <li role="menuitem"><a href="./gruppe.php"><i class="fa fa-user fa-fw"></i> Das sind wir</a></li>
+            <li role="menuitem"><a href="./termine.php"><i class="fa fa-calendar-check-o fa-fw"></i> Unsere Termine</a></li>
+            <li role="menuitem"><a href="./ausrüstung.php"><i class="fa fa-cubes fa-fw"></i> Die Ausrüstung</a></li>
+            <li role="menuitem"><a href="./historisches.php"><i class="fa fa-graduation-cap fa-fw"></i> Historisches</a></li>
+            <li role="menuitem"><a href="./social.php"><i class="fa fa-camera fa-fw"></i> Social Media</a></li>
+            <li role="menuitem"><a href="./wif/feuer.php"><i class="fa fa-thermometer-full fa-fw"></i> Wallburg in Flammen</a></li>
+            <li role="menuitem"><a href="./impressum.php"><i class="fa fa-hashtag fa-fw"></i> Impressum</a></li>
+        </ul>
+    </nav>
